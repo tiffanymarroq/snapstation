0b3d220da64b4689bf01831d50aee9b132becb18
Tiffany Marroquin <tiffany.marroq@gmail.com>
2019-06-19 17:46:48 -0400

    added new images and stuffs

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ function App() {
     <div className="App">
       <header>
         <Navbar>
-          <Nav>
-            <NavbarBrand>
-              <img className="logo" src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/slr-camera-icon-18-256.png"/>
-              <h1 className="team-name">SnapStation</h1>
-            </NavbarBrand>
-          </Nav>
+          <NavbarBrand>
+            <img  style={{marginBottom:'5px'}}className="logo" src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/slr-camera-icon-18-256.png"/>
+            <div>
+              <h1 style={{paddingLeft: '15px', marginTop:'12px'}} className="team-name">SnapStation</h1>
+              <p style={{paddingLeft: '16px'}}>For A Safe And Fun Selfie Experience</p>
+            </div>
+            
+          </NavbarBrand>
+            <div className="flex flex-center">
+              <p>Powered By</p>
+              <img className="canon-logo" src="http://pluspng.com/img-png/canon-logo-png--1200.png" alt=""/>  
+            </div>  
         </Navbar>
       </header>
       <div className="main-view ">
--- src/components/Views.js
+++ src/components/Views.js
@@ -6,36 +6,64 @@ class Views extends Component{
     constructor(props){
         super(props);
         this.state={
-            view:'startView'
+            view:'startView',
+            isOpen: false
         }
+    }
+    changeView = (view) => {
+        this.setState({
+            view: view
+        })
+    }
+
+    onRestart = () => {
+        this.setState({
+            view: 'startView'
+        })
+    }
+
+    modalToggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     }
  
     render(){
         let { view } = this.state;
         let startView = (
-                <div className="text-center start-view view flex flex-center">
                     <div className="text">
                         <h3 className="title">Welcome! </h3>
                         <p>Ready to take your pic? Click the start button below.</p>
-                        <button>Start</button>
+                        <button onClick={ () => this.changeView("imageView")}>Start</button>
                     </div>
+            )
+        let imageView = 
+            (
+                <div className="text">
+                    <h3 className="title"></h3>
+                    <p>Ready?</p>
+                    <button>Send to Friends</button>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <button onClick={this.onRestart}>Restart</button>
                 </div>
             )
-
         let displayView;
         if(view === 'startView'){
             displayView = startView;
         }else if(view === 'imageView'){
-            displayView = startView;
+            displayView = imageView;
         }else if(view === 'finalView'){
             displayView = startView;
         }else{
             displayView = startView;
         }
-        
         return(
             <div  style={{position:'relative',width:'100%',height:'100%'}}>
-                {displayView}
+                <div className={"text-center view flex flex-center " + view }>
+                    {displayView}
+                </div>
             </div>
         )
     }
