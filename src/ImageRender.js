4d6dc2625e93a1371d298d4256c399d2e42b4006
CodeSlip <jason@rawlings.io>
2019-06-20 10:47:25 -0400

    trying to get image to work

--- src/ImageRender.js
+++ src/ImageRender.js
@@ -16,51 +16,13 @@ class ImageRender extends Component{
     render(){
         let image1;
         let image2;
-        let startShooting = Axios.post('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview', {
-            "liveviewsize": "small", 
-            "cameradisplay": "on"
-        }).then(()=>{
-            setTimeout((Axios.get('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview/flip').then(
-                (image) => {
-                    this.setState({
-                        image: image1
-                    })
+        let resp = Axios.get('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview/flip').then(
+                function (resp){
+                    console.log("flip check",resp.status);
+                    console.log("flip check",resp);
+                    return resp
                 })
-                ), 1000)}
-            )
-        
-        startShooting.then((resp)=>{
-            console.log("this is what we get",resp)
-        })
-        // Axios.post('http://192.168.1.2:30011/ccapi/ver100/shooting/control/shutterbutton', {
-        //     "af": true
-        // })
-        // startShooting.then((resp)=> {
-        //     console.log("response:shooting mode",resp);
-        //     Axios.post('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview', {
-        //         "liveviewsize": "small", 
-        //         "cameradisplay": "on"
-        //     }).then((resp)=>{
-        //         console.log("response: post successful", resp)
-        //         Axios.get('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview/flip').then(
-        //             (image) => {
-        //                 console.log(image);
-        //                 image1 = image;
-        //             })
-        //         // ).then(()=> Axios.get('http://192.168.100.1:30011//ccapi/ver100/shooting/liveview/flip').then(
-        //         //     (image) => {
-        //         //         console.log(image);
-        //         //         image2 = image;
-        //         //     }
-        //         // ))
-        //     })
-        // })
-        
-        return (
-            <div>Images: <img src={this.state.image}/></div>
-        )
-    }
-
-}
+        return <div>Images: <img src={resp}/></div>
+        }}
 
 export default ImageRender;
