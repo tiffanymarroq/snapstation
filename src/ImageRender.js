9293d2ff3f36f7b8a09b3bcd6997db493f92410c
CodeSlip <jason@rawlings.io>
2019-06-20 10:00:38 -0400

    added image render, still under dev

--- src/ImageRender.js
+++ src/ImageRender.js
@@ -1,37 +1,63 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
 
+
 class ImageRender extends Component{
     constructor(props){
         super(props);
         this.state = {
-            imageCapture: []
+            image: "/assets/tajMahal.jpg"
         }
     }
 
     componentDidMount(){
-        console.log("componentdidmount began")
-        Axios.get('http://192.168.100.1:30011/ccapi/ver100/contents/sd/100CANON/')
-        .then((response) => {
-            let photos = [];
-            photos = this.state.imageCapture;
-            response.data.url.map( photo=>{
-                console.log("these are the photos", photo)
-                photos.push(photo)
-            }, () => this.setState({
-                imageCapture: photos
-            }))
-        })
     }
 
     render(){
-        let canonCapture = this.state.imageCapture;
-        let displayImages = canonCapture.map((image => {
-                return <img src={image} />
-            }))
-
+        let image1;
+        let image2;
+        let startShooting = Axios.post('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview', {
+            "liveviewsize": "small", 
+            "cameradisplay": "on"
+        }).then(()=>{
+            setTimeout((Axios.get('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview/flip').then(
+                (image) => {
+                    this.setState({
+                        image: image1
+                    })
+                })
+                ), 1000)}
+            )
+        
+        startShooting.then((resp)=>{
+            console.log("this is what we get",resp)
+        })
+        // Axios.post('http://192.168.1.2:30011/ccapi/ver100/shooting/control/shutterbutton', {
+        //     "af": true
+        // })
+        // startShooting.then((resp)=> {
+        //     console.log("response:shooting mode",resp);
+        //     Axios.post('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview', {
+        //         "liveviewsize": "small", 
+        //         "cameradisplay": "on"
+        //     }).then((resp)=>{
+        //         console.log("response: post successful", resp)
+        //         Axios.get('http://192.168.1.2:30011/ccapi/ver100/shooting/liveview/flip').then(
+        //             (image) => {
+        //                 console.log(image);
+        //                 image1 = image;
+        //             })
+        //         // ).then(()=> Axios.get('http://192.168.100.1:30011//ccapi/ver100/shooting/liveview/flip').then(
+        //         //     (image) => {
+        //         //         console.log(image);
+        //         //         image2 = image;
+        //         //     }
+        //         // ))
+        //     })
+        // })
+        
         return (
-            <div>Images: {displayImages}</div>
+            <div>Images: <img src={this.state.image}/></div>
         )
     }
 
